bab5975267a0277e9286aec6567a57366f656b03
Skeith918 <root@SKEITH-US7EKE.localdomain>
2020-11-18 18:12:05 +0100

    quiet apt

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,15 @@ function init ()
 function update ()
 {
   #SECURITY AND UPDATE
-  apt update && apt install gnupg2 software-properties-common -y && apt upgrade -y && apt install unattended-upgrades -y
-
-  #BASIC DEPENDENCIES
-  apt install git curl apt-transport-https ca-certificates libffi-dev libssl-dev python3 python3-pip -y
-  apt-get remove python-configparser
+  apt update --quiet && apt install gnupg2 software-properties-common --quiet -y && apt upgrade --quiet -y && apt install unattended-upgrades --quiet -y
 }
 
 function docker ()
 {
+  #BASIC DEPENDENCIES
+  apt install git curl apt-transport-https ca-certificates libffi-dev libssl-dev python3 python3-pip --quiet -y
+  apt-get remove python-configparser
+
   # DOCKER-CE AND DOCKER-COMPOSE INSTALLATION
   curl -sSL https://get.docker.com | sh
   pip3 install docker-compose
